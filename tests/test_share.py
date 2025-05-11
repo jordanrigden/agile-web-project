a9347dbf1a1e53b1144d4b87d80053e9d68d9b4a
Shadab <shadab0807@gmail.com>
2025-05-11 18:02:10 +0800

    Add unit test for share form

---
+++ tests/test_share.py
@@ -0,0 +1,36 @@
+import unittest
+from flask import Flask
+from forms import ShareForm
+
+class TestShareForm(unittest.TestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.config['SECRET_KEY'] = 'test'
+        self.app.config['WTF_CSRF_ENABLED'] = False
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.request_context = self.app.test_request_context()
+        self.request_context.push()
+
+    def tearDown(self):
+        self.request_context.pop()
+        self.app_context.pop()
+
+    def test_valid_share(self):
+        form = ShareForm(username='validuser', workout_id=1)
+        form.workout_id.choices = [(1, 'Workout A')]
+        self.assertTrue(form.validate())
+
+    def test_invalid_username_length(self):
+        form = ShareForm(username='abc', workout_id=1)
+        form.workout_id.choices = [(1, 'Workout A')]
+        self.assertFalse(form.validate())
+        self.assertIn('username', form.errors)
+
+        form = ShareForm(username='a' * 26, workout_id=1)
+        form.workout_id.choices = [(1, 'Workout A')]
+        self.assertFalse(form.validate())
+        self.assertIn('username', form.errors)
+
+if __name__ == "__main__":
+    unittest.main()
